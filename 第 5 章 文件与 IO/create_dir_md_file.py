f2f75c7464ac23c1e05dfb1ad3e0f0b7a87330af
L1n <490772448@qq.com>
2017-04-22 18:40:15 +0800

    加强了一下脚本, 现在可以自动更新 summary.md 了

--- 第 5 章 文件与 IO/create_dir_md_file.py
+++ 第 5 章 文件与 IO/create_dir_md_file.py
@@ -4,8 +4,36 @@
 """ 用来创建对应文件夹 及 readme.md
 """
 import os
+from itertools import dropwhile
 
 __author__ = '__L1n__w@tch'
+
+
+def update_summary_md_file(dirs):
+    # 获取当前是第几章
+    current_dir = os.path.dirname(__file__)
+    current_dir_name = os.path.basename(current_dir)
+
+    # 获取根目录路径
+    root_dir = os.path.dirname(os.path.dirname(__file__))
+
+    # 创建要更新的内容
+    update_content = list()
+    update_content.append("* [{current_dir}]({current_dir}/readme.md)\n".format(current_dir=current_dir_name))
+    for each_dir in dirs:
+        update_content.append("{sep}* [{each_dir}]({current_dir}/{each_dir}/readme.md)\n".format(
+            sep=" " * 4, each_dir=each_dir, current_dir=current_dir_name))
+
+    # 开始修改 summary.md
+    with open("{}/SUMMARY.md".format(root_dir), "rt+") as f:
+        # 跳过非本章内容, 或者直到文件结尾
+        for line in dropwhile(lambda each_line: current_dir_name not in each_line, f):
+            # 开始更新
+            # 依次判断要更新的内容是否已经存在, 存在则离开队列
+            if line == update_content[0]:
+                update_content.pop(0)
+        for each_content in update_content:
+            f.write(each_content)
 
 
 def get_all_dir_name():
@@ -19,6 +47,7 @@ def get_all_dir_name():
 
 
 def run():
+    print("[*] 开始创建文件夹及对应的 md 文件")
     # 读取同目录下的 readme.md, 获取所有文件夹名称
     dirs = get_all_dir_name()
 
@@ -31,7 +60,10 @@ def run():
             with open("{}/readme.md".format(each_dir), "xt") as f:
                 f.write("### {}".format(each_dir))
 
+    print("[*] 开始更新根目录下的 SUMMARY.md 文件")
+    update_summary_md_file(dirs)
+
 
 if __name__ == "__main__":
     run()
-    print("[*] 创建完毕")
+    print("[*] 脚本执行完毕")
